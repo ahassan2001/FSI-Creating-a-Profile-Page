91212b626f989fffb483104f03fac78489ddce63
unknown <ahassan2001@msn.com>
2022-08-30 21:05:00 -0400

    updated JS exercise

--- index.js
+++ index.js
@@ -19,4 +19,25 @@ let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
 
+let header3 = document.createElement('h3')
 
+header3.append('Description:')
+content.append(header3)
+
+let paragraph = document.createElement('p')
+paragraph.append('This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.')
+content.append(paragraph)
+
+let feedingTimes = document.createElement('h3')
+feedingTimes.append('Feeding Times:')
+content.append(feedingTimes)
+
+let user1 = document.createElement('li')
+user1.append('9:00 am')
+content.append(user1)
+let user2= document.createElement('li')
+user2.append('12:00 pm')
+content.append(user2)
+let user3 = document.createElement('li')
+user3.append('5:00 pm')
+content.append(user3)
